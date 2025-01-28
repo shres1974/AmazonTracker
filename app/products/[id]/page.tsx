dcba0f5ddbcb59c98603b62d3e685561e2612240
Nischal Shrestha <nischalshrestha@Nischals-Mac-mini.local>
2025-01-27 23:03:37 -0500

    Updated

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { redirect } from 'next/navigation';
-import Modal from '@/components/Modal';
-import PriceInfoCard from '@/components/PriceInfoCard';
-import ProductCard from '@/components/ProductCard';
-import { getProductById, getSimilarProducts } from '@/lib/actions';
-import { formatNumber } from '@/lib/utils';
-import { Product } from '@/types';
+import Modal from "@/components/Modal";
+import PriceInfoCard from "@/components/PriceInfoCard";
+import ProductCard from "@/components/ProductCard";
+import { getProductById, getSimilarProducts } from "@/lib/actions"
+import { formatNumber } from "@/lib/utils";
+import { Product } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+
 
 type Props = {
-  params: Promise<{ id: string }>
+  params: { id: string }
 }
+
 
 const ProductDetails = async ({ params }: Props) => {
   // Await params before destructuring
-  const { id } = await params;
+  const { id } =  params;
 
   // async data fetching
   const product: Product = await getProductById(id);
@@ -27,7 +29,7 @@ const ProductDetails = async ({ params }: Props) => {
     <div className="product-container">
       <div className="flex gap-28 xl:flex-row flex-col">
         <div className="product-image">
-          <Image
+          <Image 
             src={product.image}
             alt={product.title}
             width={580}
@@ -42,6 +44,7 @@ const ProductDetails = async ({ params }: Props) => {
               <p className="text-[28px] text-secondary font-semibold">
                 {product.title}
               </p>
+
               <Link
                 href={product.url}
                 target="_blank"
@@ -50,28 +53,32 @@ const ProductDetails = async ({ params }: Props) => {
                 Visit Product
               </Link>
             </div>
+
             <div className="flex items-center gap-3">
               <div className="product-hearts">
-                <Image
+                <Image 
                   src="/assets/icons/red-heart.svg"
                   alt="heart"
                   width={20}
                   height={20}
                 />
+
                 <p className="text-base font-semibold text-[#D46F77]">
                   {product.reviewsCount}
                 </p>
               </div>
+
               <div className="p-2 bg-white-200 rounded-10">
-                <Image
+                <Image 
                   src="/assets/icons/bookmark.svg"
                   alt="bookmark"
                   width={20}
                   height={20}
                 />
               </div>
+
               <div className="p-2 bg-white-200 rounded-10">
-                <Image
+                <Image 
                   src="/assets/icons/share.svg"
                   alt="share"
                   width={20}
@@ -90,10 +97,11 @@ const ProductDetails = async ({ params }: Props) => {
                 {product.currency} {formatNumber(product.originalPrice)}
               </p>
             </div>
+
             <div className="flex flex-col gap-4">
               <div className="flex gap-3">
                 <div className="product-stars">
-                  <Image
+                  <Image 
                     src="/assets/icons/star.svg"
                     alt="star"
                     width={16}
@@ -103,8 +111,9 @@ const ProductDetails = async ({ params }: Props) => {
                     {product.stars || '25'}
                   </p>
                 </div>
+
                 <div className="product-reviews">
-                  <Image
+                  <Image 
                     src="/assets/icons/comment.svg"
                     alt="comment"
                     width={16}
@@ -115,6 +124,7 @@ const ProductDetails = async ({ params }: Props) => {
                   </p>
                 </div>
               </div>
+
               <p className="text-sm text-black opacity-50">
                 <span className="text-primary-green font-semibold">93% </span> of
                 buyers have recommended this.
@@ -124,22 +134,22 @@ const ProductDetails = async ({ params }: Props) => {
 
           <div className="my-7 flex flex-col gap-5">
             <div className="flex gap-5 flex-wrap">
-              <PriceInfoCard
+              <PriceInfoCard 
                 title="Current Price"
                 iconSrc="/assets/icons/price-tag.svg"
                 value={`${product.currency} ${formatNumber(product.currentPrice)}`}
               />
-              <PriceInfoCard
+              <PriceInfoCard 
                 title="Average Price"
                 iconSrc="/assets/icons/chart.svg"
                 value={`${product.currency} ${formatNumber(product.averagePrice)}`}
               />
-              <PriceInfoCard
+              <PriceInfoCard 
                 title="Highest Price"
                 iconSrc="/assets/icons/arrow-up.svg"
                 value={`${product.currency} ${formatNumber(product.highestPrice)}`}
               />
-              <PriceInfoCard
+              <PriceInfoCard 
                 title="Lowest Price"
                 iconSrc="/assets/icons/arrow-down.svg"
                 value={`${product.currency} ${formatNumber(product.lowestPrice)}`}
@@ -156,27 +166,30 @@ const ProductDetails = async ({ params }: Props) => {
           <h3 className="text-2xl text-secondary font-semibold">
             Product Description
           </h3>
+
           <div className="flex flex-col gap-4">
             {product?.description?.split('\n')}
           </div>
         </div>
 
         <button className="btn w-fit mx-auto flex items-center justify-center gap-3 min-w-[200px]">
-          <Image
+          <Image 
             src="/assets/icons/bag.svg"
             alt="check"
             width={22}
             height={22}
           />
-          <Link href={product.url} className="text-base text-white">
+
+          <Link href="/" className="text-base text-white">
             Buy Now
           </Link>
         </button>
       </div>
 
-      {similarProducts && similarProducts.length > 0 && (
+      {similarProducts && similarProducts?.length > 0 && (
         <div className="py-14 flex flex-col gap-2 w-full">
           <p className="section-text">Similar Products</p>
+
           <div className="flex flex-wrap gap-10 mt-7 w-full">
             {similarProducts.map((product) => (
               <ProductCard key={product._id} product={product} />
